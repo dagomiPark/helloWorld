b8ea6cf1139ddef36b73172030d6b6ddd5dc60b6
parkseungwon <i.am.psw86@gmail.com>
2016-10-15 01:23:12 +0900

    eee

--- MyApplication/app/src/main/java/com/example/park2/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/park2/myapplication/MainActivity.java
@@ -7,22 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends AppCompatActivity {
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        // hihihih
     }
 
     public void btnClickEvent1(View view){
